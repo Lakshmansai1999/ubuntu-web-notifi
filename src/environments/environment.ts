14d40ef102dc852c5124aa5f1616c41f70b11a03
Lakshmansai1999 <klakshmansai@stratapps.com>
2023-11-15 19:22:44 +0530

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyD6EnFDvEc9EyEVITUQPNZDMbFa4ZrFxz8",
-    authDomain: "angular-phone-authentication.firebaseapp.com",
-    databaseURL: "https://angular-phone-authentication.firebaseio.com",
-    projectId: "angular-phone-authentication",
-    storageBucket: "angular-phone-authentication.appspot.com",
-    messagingSenderId: "636420904856",
-    appId: "1:636420904856:web:f9cb9810cf99fe0d3cbded",
-    measurementId: "G-V3HR912KFJ"
+    apiKey: "AIzaSyDNzK-TGv2GtK6MmQurjGB_xba5_fY7rto",
+    authDomain: "web-notifications-8a6a3.firebaseapp.com",
+    projectId: "web-notifications-8a6a3",
+    storageBucket: "web-notifications-8a6a3.appspot.com",
+    messagingSenderId: "1004305289396",
+    appId: "1:1004305289396:web:15993cefcdcf3631be07e1",
+    measurementId: "G-S1YDQEW6QM",
+    vapidKey:"BAgZ1XZ8oY978Vy8jMkYNISAyICjn58viLy7Z_KqI_N6zrd-hU-hURdnw0ziqIbFOVMMhB0XCY1G0vWXtnfnLAU"
   }
 };
 
--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -2,14 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.0.0/firebase-messaging.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyD6EnFDvEc9EyEVITUQPNZDMbFa4ZrFxz8",
-  authDomain: "angular-phone-authentication.firebaseapp.com",
-  databaseURL: "https://angular-phone-authentication.firebaseio.com",
-  projectId: "angular-phone-authentication",
-  storageBucket: "angular-phone-authentication.appspot.com",
-  messagingSenderId: "636420904856",
-  appId: "1:636420904856:web:f9cb9810cf99fe0d3cbded",
-  measurementId: "G-V3HR912KFJ"
+  apiKey: "AIzaSyDNzK-TGv2GtK6MmQurjGB_xba5_fY7rto",
+  authDomain: "web-notifications-8a6a3.firebaseapp.com",
+  projectId: "web-notifications-8a6a3",
+  storageBucket: "web-notifications-8a6a3.appspot.com",
+  messagingSenderId: "1004305289396",
+  appId: "1:1004305289396:web:15993cefcdcf3631be07e1",
+  measurementId: "G-S1YDQEW6QM"
 });
 
 const messaging = firebase.messaging();
